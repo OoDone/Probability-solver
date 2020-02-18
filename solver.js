b3b8e817bf4a251f93b4edcc43664e4b96902220
OoDone <48130949+OoDone@users.noreply.github.com>
2020-02-18 13:03:29 -0500

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,5 +1,7 @@
-var INPUT = process.argv['2'];
-console.log(INPUT);
+var INPUT = process.argv;
+var opt = INPUT['2'];
+var stat = INPUT['3'];
+solver(opt, stat);
 
 function solver(options, staticrepeat) {
   var answer = (1 / options)^staticrepeat;
